a9f74bb8193876d7c32bfe4c6e5840e64e90ba85
Prakash105 <prakashmadav97@gmail.com>
2022-05-03 17:48:03 +0530

    GIT

---
+++ src/test/java/org/git/GIT.java
@@ -0,0 +1,19 @@
+package org.git;
+
+public class GIT {
+private void meth1() {
+	System.out.println("1");
+}
+private void meth2() {
+	System.out.println("2");
+}
+private void meth3() {
+	System.out.println("3");
+}
+private void meth4() {
+	System.out.println("4");
+}
+private void meth5() {
+	System.out.println("5");
+}
+}
